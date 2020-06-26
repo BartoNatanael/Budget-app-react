d49bac56858bfa7be13e454cc67f7d2a280fbfd1
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-06-26 17:17:23 +0200

    Nav comp added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
-// import logo from './logo.svg';
-import {ThemeProvider} from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import {ThemeProvider } from 'styled-components';
 import theme from 'utils/theme';
-import './App.css';
+import GlobalStyles from './index.css.js';
 
 import { Navigation } from 'components';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Navigation/>
-      </div>
+      <GlobalStyles/>
+      <Router>
+        <Navigation items={[
+          {content: 'Homepage', to: '/'},
+          {content: 'Budget', to: '/budget'}
+        ]}/>
+        <Switch>
+          <Route exact path="/">
+            Hompepage
+          </Route>
+          <Route path="/budget">
+            Budget page
+          </Route>
+        </Switch>
+      </Router>
+
     </ThemeProvider>
   );
 }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { styled } from 'styled-components';
+import {Link} from 'react-router-dom';
 
-const 
+import { Wrapper } from 'components';
 
-function Navigation(){
+import {Container, List} from './Navigation.css';
+
+function Navigation({items}){
     return(
-        <div>Text</div>
+        <Container>
+            <Wrapper>
+                <List>
+                    {items.map(item => (
+                        <li key={item.to}>
+                            <Link to={item.to}>{item.content}</Link>
+                            </li>
+                    ))}
+                </List>
+            </Wrapper>
+        </Container>
     )
 }
 
