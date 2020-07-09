dd1a6d0cc1927a41a329ee43c92ecbc00ea3c51a
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-07-09 10:51:59 +0200

    working on redux

--- src/data/reducers/budget.reducer.js
+++ src/data/reducers/budget.reducer.js
@@ -3,7 +3,10 @@ import {
     BUDGET_GET_REQUEST,
     BUDGET_GET_SUCCESS,
     BUDGET_GET_FAILURE,
-    LOADING_STATES
+    LOADING_STATES,
+    BUDGETED_CATEGORIES_GET_REQUEST,
+    BUDGETED_CATEGORIES_GET_SUCCESS,
+    BUDGETED_CATEGORIES_GET_FAILURE,
 } from 'data/constants';
 
 const initialState = {
@@ -36,9 +39,32 @@ function budget(state = initialState, action){
             delete newLoadingState.BUDGET_GET_REQUEST;
             return {
                 ...state,
-                budget: {},
+                budget: [],
                 loadingState: newLoadingState
-            }
+            };
+
+            case BUDGETED_CATEGORIES_GET_REQUEST:
+                return {
+                    ...state,
+                    loadingState:{
+                        ...state.loadingState,
+                        [action.type]: LOADING_STATES.LOADING,
+                    }
+                };
+            case BUDGETED_CATEGORIES_GET_SUCCESS:
+                delete newLoadingState.BUDGETED_CATEGORIES_GET_REQUEST;
+                return {
+                    ...state,
+                    budgetedCategories: action.payload,
+                    loadingState: newLoadingState,
+                };
+            case BUDGETED_CATEGORIES_GET_FAILURE:
+                delete newLoadingState.BUDGETED_CATEGORIES_GET_REQUEST;
+                return {
+                    ...state,
+                    budgetedCategories: [],
+                    loadingState: newLoadingState
+                }
         default:
             return state;
     }
--- src/data/store.js
+++ src/data/store.js
@@ -1,20 +1,23 @@
-import { applyMiddleware, createStore } from 'redux'
+import {applyMiddleware, createStore} from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './reducers'
 
 export default function configureStore(preloadedState) {
-  const middlewares = [
-  ];
-  const middlewareEnhancer = applyMiddleware(...middlewares);
-  const enhancers = [middlewareEnhancer];
+  const middlewares = [ 
+      thunkMiddleware
+]
+  const middlewareEnhancer = applyMiddleware(...middlewares)
 
-  const composedEnhancers = composeWithDevTools(...enhancers);
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const enhancers = [middlewareEnhancer]
+  const composedEnhancers = composeWithDevTools(...enhancers)
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers)
 
   if (process.env.NODE_ENV !== 'production' && module.hot) {
-    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+    module.hot.accept('data/reducers', () => store.replaceReducer(rootReducer));
   }
 
-  return store;
+  return store
 }
