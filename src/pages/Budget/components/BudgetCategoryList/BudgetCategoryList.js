18c8377fa51b59aca54e9bc2e08a4fa2684e6fab
BartoNatanael <bartlomiej.krzysztof.nowak@gmail.com>
2020-07-21 11:15:26 +0200

    its nice

--- src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
+++ src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
@@ -1,11 +1,15 @@
 import React, { useMemo } from 'react';
 import { connect } from 'react-redux';
 import { groupBy } from 'lodash';
+import { useTranslation } from 'react-i18next';
+import 'styled-components/macro';
+
 import {ToggleableList} from 'components';
 import ParentCategory from './ParentCategory';
 import CategoryItem from './CategoryItem';
 
 function BudgetCategoryList({ budgetedCategories, allCategories, budget}) {
+    const { t } = useTranslation();
 
     const budgetedCategoriesByParent = useMemo(
         () => groupBy(
@@ -41,16 +45,52 @@ function BudgetCategoryList({ budgetedCategories, allCategories, budget}) {
         .reduce((acc, transaction) => acc + transaction.amount, 0);
 
     const restToSpent = budget.totalAmount - totalSpent;
+    const amountTaken = budgetedCategories.reduce((acc, budgetedCategory) => {
+        const categoryTransactions = budget.transactions
+            .filter(transaction => transaction.id === budgetedCategory.id);
+        const categoryExpenses = categoryTransactions
+            .reduce((acc, transaction)=> acc + transaction.amount, 0);
+
+        return acc + Math.max(categoryExpenses, budgetedCategory.budget);
+    }, 0);
+
+    const notBudgetedTransaction = budget.transactions
+        .filter(transaction => {
+            return !budgetedCategories
+                .find(budgetedCategory=> budgetedCategory.id === transaction.categoryId)
+            });
+    const notBudgetedExpenses = notBudgetedTransaction.reduce((acc, transaction) => acc + transaction.amount, 0);
+    
+    const availableForRestCategories = budget.totalAmount - amountTaken - notBudgetedExpenses;
+
+    console.log(availableForRestCategories)
     
     return(
         <div>
-            <ParentCategory
+            <div
+                css={`
+                    border-bottom: 5px solid ${({theme}) => theme.colors.gray.light}
+                `}
+            >
+                <ParentCategory
                 name={budget.name}
                 amount={restToSpent}
             />
+            </div>
+            
             <ToggleableList
                 items={listItems}
             />
+            <div
+                css={`
+                border-top: 5px solid ${({theme}) => theme.colors.gray.light}
+            `}
+            >
+                <ParentCategory
+                name={t('Other Categories')}
+                amount={availableForRestCategories}
+            />
+            </div>
         </div>
     )
 }
