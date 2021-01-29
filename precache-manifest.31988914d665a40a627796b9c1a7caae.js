ef0ee01da64e7a704cdd4fcb3a7611395aaea7c7
admin <admin@Mobifans-MacBook-Pro.local>
2021-01-29 21:07:09 +0100

    Updates

---
+++ precache-manifest.31988914d665a40a627796b9c1a7caae.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd183e39b61c835435dfc763ce1347a",
+    "url": "/Budget-app-react/index.html"
+  },
+  {
+    "revision": "4c49dd654f5a7bb0a49a",
+    "url": "/Budget-app-react/static/css/4.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b15064537d53e683ea7b",
+    "url": "/Budget-app-react/static/js/0.0d7102c5.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/Budget-app-react/static/js/0.0d7102c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b19db93d405967e104",
+    "url": "/Budget-app-react/static/js/1.d720688d.chunk.js"
+  },
+  {
+    "revision": "d808644aa430d9c0c795",
+    "url": "/Budget-app-react/static/js/10.414124ac.chunk.js"
+  },
+  {
+    "revision": "27a7716b94c1357acffe",
+    "url": "/Budget-app-react/static/js/11.460f87dd.chunk.js"
+  },
+  {
+    "revision": "4c49dd654f5a7bb0a49a",
+    "url": "/Budget-app-react/static/js/4.ff6bb868.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Budget-app-react/static/js/4.ff6bb868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115012a52e149b50ff5f",
+    "url": "/Budget-app-react/static/js/5.c48dda2d.chunk.js"
+  },
+  {
+    "revision": "188ef96bab0b76c0d92e",
+    "url": "/Budget-app-react/static/js/6.649c0482.chunk.js"
+  },
+  {
+    "revision": "95d4d6663c18e37f1135",
+    "url": "/Budget-app-react/static/js/7.940fe6ad.chunk.js"
+  },
+  {
+    "revision": "135e531a3325830d8966",
+    "url": "/Budget-app-react/static/js/8.2bf00cb9.chunk.js"
+  },
+  {
+    "revision": "554f2522c93627007b0c",
+    "url": "/Budget-app-react/static/js/9.8bfb9a17.chunk.js"
+  },
+  {
+    "revision": "6d72f67a134ed05b7f07",
+    "url": "/Budget-app-react/static/js/main.ca72f009.chunk.js"
+  },
+  {
+    "revision": "85414696991ab76abd74",
+    "url": "/Budget-app-react/static/js/runtime-main.c9145a06.js"
+  }
+]);
